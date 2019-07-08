04a139184b4359d8dfcaae3afb05ed9a058d98d3
RobocopMao <410503915@qq.com>
2019-07-08 17:14:12 +0800

    增加小程序更新提示

--- src/app.js
+++ src/app.js
@@ -34,6 +34,33 @@ class App extends Component {
         'desc': "你的位置信息将用于小程序查询天气"
       }
     }
+  };
+
+  componentWillMount() {
+    const updateManager = Taro.getUpdateManager();
+    updateManager.onCheckForUpdate(function (res) {
+      // 请求完新版本信息的回调
+      //   console.log(res.hasUpdate)
+    });
+    updateManager.onUpdateReady(function () {
+      Taro.showModal({
+        title: '更新提示',
+        content: '新版本已经准备好，是否重启应用？',
+        success: function (res) {
+          if (res.confirm) {
+            // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+            updateManager.applyUpdate();
+          }
+        }
+      })
+
+    });
+    updateManager.onUpdateFailed(function () {
+      Taro.showToast({
+        title: '有新的版本存在,但下载失败,请将小程序从列表中删除再重新搜索该小程序进入',
+        icon: 'loading'
+      })
+    });
   }
 
   componentDidMount () {}
