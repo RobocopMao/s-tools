f4895d5d51c5515cf1a5afce49a81475a11e1f21
RobocopMao <410503915@qq.com>
2019-07-14 10:54:34 +0800

    福利养眼图优化，调用小程序接口预览图片，修改获取图片数据采用随机获取

--- src/apis/girls.js
+++ src/apis/girls.js
@@ -2,10 +2,19 @@ import Request from './request';
 
 const request = new Request();
 
-// 查询福利图列表
+// 查询福利图列表，分页
 export const getGirlsImgList = async (data) => {
   return await request.get({
     url: '/image/girl/list',
+    data,
+    loading: false
+  });
+};
+
+// 查询福利图列表随机
+export const getGirlsImgListRandom = async (data) => {
+  return await request.get({
+    url: '/image/girl/list/random',
     data,
     loading: false
   });
--- src/pages/girls/girls.js
+++ src/pages/girls/girls.js
@@ -1,36 +1,21 @@
 import Taro, {useEffect, useState} from '@tarojs/taro'
 import {View, Text, ScrollView, Image, Button} from '@tarojs/components'
-import { getGirlsImgList } from '../../apis/girls'
+import moment from 'moment'
+import { getGirlsImgList, getGirlsImgListRandom } from '../../apis/girls'
 import { useAsyncEffect } from '../../utils';
 import downloadImg from '../../assets/images/download.png';
 import './girls.scss'
 
 function Girls() {
-  const [page, setPage] = useState(1);
-  const [totalPage, setTotalPage] = useState(1); // 总页数
+  // const [page, setPage] = useState(1);
+  // const [totalPage, setTotalPage] = useState(1); // 总页数
   const [isLoading, setIsLoading] = useState(false); // 加载提示
   const [col1, setCol1] = useState([]);
   const [col2, setCol2] = useState([]);
 
-  useAsyncEffect(async () => {
-    setIsLoading(true);
-    let res = await getGirlsImgList({page});
-    let _col1 = [];
-    let _col2 = [];
-    for (let [i, v] of res.list.entries()) {
-      if ((i + 1) % 2 !== 0) { // 奇数
-        _col1.push(v);
-      } else {
-        _col2.push(v);
-      }
-    }
-    let _col11 = col1.concat(_col1);
-    let _col21 = col2.concat(_col2);
-    setCol1(_col11);
-    setCol2(_col21);
-    setTotalPage(res.totalPage);
-    setIsLoading(false);
-  }, [page]);
+  useEffect(() => {
+    getImgList();
+  }, []);
 
   const [scrollHeight, setScrollHeight] = useState(0); // 可使用窗口高度
 
@@ -50,52 +35,82 @@ function Girls() {
     });
   }, []);
 
+  // 随机获取图片
+  const getImgList = async () => {
+    setIsLoading(true);
+    let res = await getGirlsImgListRandom();
+    let _col1 = [];
+    let _col2 = [];
+    for (let [i, v] of res.entries()) {
+      if ((i + 1) % 2 !== 0) { // 奇数
+        _col1.push(v);
+      } else {
+        _col2.push(v);
+      }
+    }
+    let _col11 = col1.concat(_col1);
+    let _col21 = col2.concat(_col2);
+    setCol1(_col11);
+    setCol2(_col21);
+    // setTotalPage(res.totalPage);
+    setIsLoading(false);
+  };
+
   // 滑动到底部的事件处理函数
   const scrollToLower = () => {
     // 设置page
-    setPage(prevPage => (prevPage === totalPage ? totalPage : prevPage + 1));
+    // setPage(prevPage => (prevPage === totalPage ? totalPage : prevPage + 1));
+    getImgList();
   };
 
-  const saveImageToPhotosAlbum = (url) => {
-    Taro.getSetting({ // 获取设置
-      success(res) {
-        if (!res.authSetting['scope.writePhotosAlbum']) {
-          Taro.authorize({ // 相册授权
-            scope: 'scope.writePhotosAlbum',
-            success() {
-              saveImage(url);
-            },
-            fail() {
-              Taro.showToast({title: '相册授权失败，请在设置里面开启', icon: 'none'});
-            }
-          })
-        } else {
-          saveImage(url);
-        }
-      }
-    })
-  };
+  // const saveImageToPhotosAlbum = (url) => {
+  //   Taro.getSetting({ // 获取设置
+  //     success(res) {
+  //       if (!res.authSetting['scope.writePhotosAlbum']) {
+  //         Taro.authorize({ // 相册授权
+  //           scope: 'scope.writePhotosAlbum',
+  //           success() {
+  //             saveImage(url);
+  //           },
+  //           fail() {
+  //             Taro.showToast({title: '相册授权失败，请在设置里面开启', icon: 'none'});
+  //           }
+  //         })
+  //       } else {
+  //         saveImage(url);
+  //       }
+  //     }
+  //   })
+  // };
 
   // 保存图片
-  const saveImage = (url) => {
-    Taro.getImageInfo({  // 获取图片信息
-      src: url,
-      success (res1) {
-        console.log(res1);
-        Taro.saveImageToPhotosAlbum({  // 保存图片到相册
-          filePath: res1.path,
-          success(res2) {
-            Taro.showToast({title: '保存图片成功', icon: 'none'});
-          },
-          fail() {
-            Taro.showToast({title: '保存图片失败', icon: 'none'});
-          }
-        })
-      },
-      fail() {
-        Taro.showToast({title: '获取网络图片数据失败', icon: 'none'});
-      }
-    })
+  // const saveImage = (url) => {
+  //   Taro.getImageInfo({  // 获取图片信息
+  //     src: url,
+  //     success (res1) {
+  //       console.log(res1);
+  //       Taro.saveImageToPhotosAlbum({  // 保存图片到相册
+  //         filePath: res1.path,
+  //         success(res2) {
+  //           Taro.showToast({title: '保存图片成功', icon: 'none'});
+  //         },
+  //         fail() {
+  //           Taro.showToast({title: '保存图片失败', icon: 'none'});
+  //         }
+  //       })
+  //     },
+  //     fail() {
+  //       Taro.showToast({title: '获取网络图片数据失败', icon: 'none'});
+  //     }
+  //   })
+  // };
+
+  // 预览图片
+  const previewImg = (url) => {
+    Taro.previewImage({
+      current: url,
+      urls: [url]
+    });
   };
 
   return (
@@ -110,15 +125,15 @@ function Girls() {
         <View className='flex-column col1'>
           {col1.map((img, index) => {
             return (
-              <View className='bd-box of-hidden img-box relative' key={img.imageUrl + '_col1'}>
-                <View className='mg-t-10 mg-l-10 of-hidden'>
+              <View className='bd-box of-hidden img-box relative' key={img.imageUrl + '_col1_' + index + '_' + moment().format('x')}>
+                <View className='mg-t-10 mg-l-10 of-hidden' onClick={() => previewImg(img.imageUrl)}>
                   <Image className='img' src={img.imageUrl} />
                 </View>
-                <View className='w68 h68 dl-btn'>
-                  <Button className='w68 h68 circle bd-no pd-0  flex-row flex-col-center flex-row-center bg-no' onClick={() => saveImageToPhotosAlbum(img.imageUrl)}>
-                    <Image className='w60 h60' src={downloadImg} />
-                  </Button>
-                </View>
+                {/*<View className='w68 h68 dl-btn'>*/}
+                  {/*<Button className='w68 h68 circle bd-no pd-0  flex-row flex-col-center flex-row-center bg-no' onClick={() => saveImageToPhotosAlbum(img.imageUrl)}>*/}
+                    {/*<Image className='w60 h60' src={downloadImg} />*/}
+                  {/*</Button>*/}
+                {/*</View>*/}
               </View>
             )
           })}
@@ -126,23 +141,23 @@ function Girls() {
         <View className='flex-column col2'>
           {col2.map((img, index) => {
             return (
-              <View className='bd-box of-hidden img-box relative' key={img.imageUrl + '_col2'}>
-                <View className='mg-t-10 mg-l-10 mg-r-10 of-hidden'>
+              <View className='bd-box of-hidden img-box relative' key={img.imageUrl + '_col2_' + index + '_' + moment().format('x')}>
+                <View className='mg-t-10 mg-l-10 mg-r-10 of-hidden' onClick={() => previewImg(img.imageUrl)}>
                   <Image className='img' src={img.imageUrl} />
                 </View>
-                <View className='w68 h68 dl-btn'>
-                  <Button className='w68 h68 circle bd-no pd-0  flex-row flex-col-center flex-row-center bg-no' onClick={() => saveImageToPhotosAlbum(img.imageUrl)}>
-                    <Image className='w60 h60' src={downloadImg} />
-                  </Button>
-                </View>
+                {/*<View className='w68 h68 dl-btn'>*/}
+                  {/*<Button className='w68 h68 circle bd-no pd-0  flex-row flex-col-center flex-row-center bg-no' onClick={() => saveImageToPhotosAlbum(img.imageUrl)}>*/}
+                    {/*<Image className='w60 h60' src={downloadImg} />*/}
+                  {/*</Button>*/}
+                {/*</View>*/}
               </View>
             )
           })}
         </View>
       </View>
-      <View className='flex-row flex-row-center font26 pd-t-20 pd-b-20'>
-        {isLoading && page !== totalPage && <Text>美女正在赶来...</Text>}
-        {page === totalPage && <Text>哇！美女被你看光了</Text>}
+      <View className='flex-row flex-row-center font26 pd-t-40 pd-b-40'>
+        {isLoading && <Text>一大波美女正向你扑来...</Text>}
+        {/*{page === totalPage && <Text>哇！美女被你看光了</Text>}*/}
       </View>
     </ScrollView>
 
