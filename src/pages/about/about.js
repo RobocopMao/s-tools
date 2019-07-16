cfd28cceea38116e52cdf7e9376d106a21136ad5
RobocopMao <410503915@qq.com>
2019-07-16 17:43:20 +0800

    关于和跟新日志修改

--- src/pages/about/about.js
+++ src/pages/about/about.js
@@ -1,5 +1,5 @@
 import Taro, {useEffect, useState} from '@tarojs/taro'
-import {View, Text, ScrollView} from '@tarojs/components'
+import {View, Text, ScrollView, Navigator} from '@tarojs/components'
 import _updateLog from '../../apis/update_log.json';
 import './about.scss'
 
@@ -37,7 +37,7 @@ function About() {
         <Text className='pd-20'>1.查询天气需要获取用户位置信息，请确保微信和小程序有使用定位的权限。</Text>
         <Text className='pd-20'>2.万能方法：升级小程序后导致权限获取失败，请在微信-发现-小程序里面删除小程序后再添加小程序。</Text>
         <Text className='pd-20 black bold'>使用方法与说明：</Text>
-        <Text className='pd-20'>1.养眼福利图：可以在预览界面长按后转发、保存、收藏。</Text>
+        <View className='pd-20'><Text>1.</Text><Navigator className='blue inline-block' url='/pages/girls/girls'>养眼福利图</Navigator><Text>：可以在预览界面长按后转发、保存、收藏。</Text></View>
         <Text className='pd-20'>2.万年历：可以查看从本月起一年的日历，日历可以竖向滚动，下一年的日历信息需要等待接口更新。</Text>
         <Text className='pd-20 black bold'>更新日志：</Text>
         <View className='pd-20 flex-column mg-b-20'>
@@ -57,7 +57,6 @@ function About() {
                         <Text className='mg-b-10' key={_index}>{_index + 1}.{_log}。</Text>
                       )
                     })}
-
                   </View>
                 </View>
               </View>
