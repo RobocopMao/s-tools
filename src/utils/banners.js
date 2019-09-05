77bcea1069550e07485712bcb75255275adb3de9
RobocopMao <410503915@qq.com>
2019-09-05 14:07:54 +0800

    首页新增两套皮肤，现在一共10套皮肤

---
+++ src/utils/banners.js
@@ -0,0 +1,24 @@
+import bannerImg1 from '../assets/images/banner1.jpg';
+import bannerImg2 from '../assets/images/banner2.jpg';
+import bannerImg3 from '../assets/images/banner3.jpg';
+import bannerImg4 from '../assets/images/banner4.jpg';
+import bannerImg5 from '../assets/images/banner5.jpg';
+import bannerImg6 from '../assets/images/banner6.jpg';
+import bannerImg7 from '../assets/images/banner7.jpg';
+import bannerImg8 from '../assets/images/banner8.jpg';
+import bannerImg9 from '../assets/images/banner9.jpg';
+import bannerImg10 from '../assets/images/banner10.jpg';
+
+const banners = [
+  {img: bannerImg1, color: '#1E154D'},
+  {img: bannerImg2, color: '#FFC103'},
+  {img: bannerImg3, color: '#504dbe'},
+  {img: bannerImg4, color: '#844DF6'},
+  {img: bannerImg5, color: '#fefdfe'},
+  {img: bannerImg6, color: '#8DB224'},
+  {img: bannerImg7, color: '#01479d'},
+  {img: bannerImg8, color: '#FFA6B6'},
+  {img: bannerImg9, color: '#000000'},
+  {img: bannerImg10, color: '#EFE5E5'}];
+
+export default banners;
