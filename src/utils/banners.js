6f5af6431a35ba76a3d2ba7011b86110025a5fa2
RobocopMao <410503915@qq.com>
2019-11-23 23:33:56 +0800

    新增2020元旦和春节特定皮肤

--- src/utils/banners.js
+++ src/utils/banners.js
@@ -9,6 +9,8 @@ import bannerImg8 from '../pages/home/assets/images/banner8.jpg';
 import bannerImg9 from '../pages/home/assets/images/banner9.jpg';
 import bannerImg10 from '../pages/home/assets/images/banner10.jpg';
 import bannerImg11 from '../pages/home/assets/images/banner11.jpg';
+import bannerImg12 from '../pages/home/assets/images/banner12.jpg';
+import bannerImg13 from '../pages/home/assets/images/banner13.jpg';
 
 const bannersConfig = [
   {img: bannerImg1, color: '#1E154D', colorType: 'dark'},
@@ -21,6 +23,9 @@ const bannersConfig = [
   {img: bannerImg8, color: '#FFA6B6', colorType: 'dark'},
   {img: bannerImg9, color: '#000000', colorType: 'dark'},
   {img: bannerImg10, color: '#EFE5E5', colorType: 'light'},
-  {img: bannerImg11, color: '#AD1D26', colorType: 'dark'}];
+  {img: bannerImg11, color: '#AD1D26', colorType: 'dark'},
+  {img: bannerImg12, color: '#fcbf82', colorType: 'light'},
+  {img: bannerImg13, color: '#E03447', colorType: 'dark'},
+];
 
 export default bannersConfig;
